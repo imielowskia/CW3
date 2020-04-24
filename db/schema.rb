fb799cbc318cb7084376e12729389a7a8922704a
imielowskia <andrzej.imielowski@pwste.edu.pl>
2020-04-24 14:40:22 +0200

    po drobnych poprawkach

--- db/schema.rb
+++ db/schema.rb
@@ -30,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_04_11_133248) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
