d5e7c34d0a27ff6628eb795e63f3a42a5d690a16
imielowskia <andrzej.imielowski@pwste.edu.pl>
2020-05-04 00:44:15 +0200

    sortowanie,edycja,lista zapisów

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 import "bootstrap"
 
@@ -16,3 +17,5 @@ import "bootstrap"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+window.jQuery = $;
+window.$ = $;
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'courses/az' , as: :course_az
+  get 'courses/za' , as: :course_za
+
+  get 'courses/:id/list' => 'courses#list', as: :course_list
+  get 'courses/:id/remove/:student_id' => 'courses#remove', as: :course_remove
+  get 'courses/:id/append/:student_id' => 'courses#append', as: :course_append
+
   resources :courses
   resources :groups
   resources :students
